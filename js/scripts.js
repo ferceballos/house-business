32e3761b999d7acd6c4cdff065e169738cd70ff1
Fer Ceballos <unceballos@gmail.com>
2017-12-24 01:41:16 -0600

    Added sass variables; improved the way the homepage is displayed

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function (e) {
+$(document).ready(function (e) {    
     $('.search-panel .dropdown-menu').find('button').click(function (e) {
         e.preventDefault();
         var param = $(this).attr("href").replace("#", "");
@@ -6,4 +6,19 @@ $(document).ready(function (e) {
         $('.search-panel span#search_concept').text(concept);
         $('.input-group #search_param').val(param);
     });
+
+
+
+    
+    $('#searchBtn').click(function () {
+        console.log('getting into function')
+        $('#title').removeClass('fadeInUp');
+        $('#title').addClass('fadeInUp');
+    })
+
+    
 });
+
+
+
+
