e6fb59b7024281e900a78b296c456be243466676
Fer Ceballos <unceballos@gmail.com>
2017-12-28 14:35:32 -0600

    Homepage animations and redesigned, also a login page was added

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,31 @@
 $(document).ready(function (e) {    
+
+    $('#dropdown-menu').click(function () {
+        $('#dropdown-menu').removeClass('fadeInDown');
+        $('#dropdown-menu').addClass('fadeInDown');
+
+        $('#dropdown-menu').removeClass('dropfix');
+        $('#dropdown-menu').addClass('dropfix');
+    })
+
+    // Lo que hace que los filtros del buscador funcionen
     $('.search-panel .dropdown-menu').find('button').click(function (e) {
         e.preventDefault();
         var param = $(this).attr("href").replace("#", "");
         var concept = $(this).text();
         $('.search-panel span#search_concept').text(concept);
         $('.input-group #search_param').val(param);
+
     });
 
 
+    //Animaciones cuando la pagina carga
+    $(document).ready(function () {
+        console.log('entrando a el loaded page.')
+        $('#header').addClass('fadeInDown');
+        $('#title').addClass('fadeInDown');
+        $('#subtitle').addClass('fadeInUp');
 
-    
-    $('#searchBtn').click(function () {
-        console.log('getting into function')
-        $('#title').removeClass('fadeInUp');
-        $('#title').addClass('fadeInUp');
     })
 
     
